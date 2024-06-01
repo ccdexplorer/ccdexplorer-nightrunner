74c18659552906f2aa3aa45773e327d72dab12de
Sander de Ruiter <sdr@16april1973.nl>
2024-06-01 15:05:54 +0200

    Fixes

--- main.py
+++ main.py
@@ -29,42 +29,42 @@ motormongo = MongoMotor(tooter)
 async def main():
     night_runner = NightRunner(grpcclient, tooter, mongodb, motormongo)
     async with AsyncScheduler() as scheduler:
-        await scheduler.add_schedule(
-            night_runner.repo_pull, IntervalTrigger(seconds=60)
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_daily_holders,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_daily_limits,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_network,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_classified_pools,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_microccd,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_ccd_classified,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_exchange_wallets,
-            IntervalTrigger(seconds=5 * 60),
-        )
+        # await scheduler.add_schedule(
+        #     night_runner.repo_pull, IntervalTrigger(seconds=60)
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_daily_holders,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_daily_limits,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_network,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_classified_pools,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_microccd,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_ccd_classified,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_exchange_wallets,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
         await scheduler.add_schedule(
             night_runner.perform_statistics_release_amounts,
             IntervalTrigger(seconds=5 * 60),
         )
-        # fmt: off
+        # # fmt: off
         await scheduler.add_schedule(
             night_runner.perform_statistics_mongo_transactions,
             IntervalTrigger(seconds=5 * 60),
@@ -77,20 +77,19 @@ async def main():
             night_runner.perform_statistics_transaction_fees,
             IntervalTrigger(seconds=5 * 60),
         )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_historical_exchange_rates,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_bridges_and_dexes,
-            IntervalTrigger(seconds=5 * 60),
-        )
-        await scheduler.add_schedule(
-            night_runner.perform_statistics_transaction_types,
-            IntervalTrigger(seconds=5 * 60),
-        )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_historical_exchange_rates,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_bridges_and_dexes,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
+        # await scheduler.add_schedule(
+        #     night_runner.perform_statistics_transaction_types,
+        #     IntervalTrigger(seconds=5 * 60),
+        # )
         await scheduler.run_until_stopped()
-        pass
 
 
 if __name__ == "__main__":
